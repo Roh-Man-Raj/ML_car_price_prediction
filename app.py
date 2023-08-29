d0d9720cf367a690b72154f8de89cdc5a2fe3f6d
rohitsoni0805@gmail.com <122146631+Rohitsoni-halo@users.noreply.github.com>
2023-08-29 18:38:31 +0530

    python file for the web application of car price sale model

--- app.py
+++ app.py
@@ -69,20 +69,51 @@ fuel_system= st.radio("Fuel System", ('MPFI','2BBL','MFI','1BBL','SPFI','4BBL','
 fuel_dict={'1BBL':0,'2BBL':1,'4BBL':2,'ID':3,'MFI':4,'MPFI':5,'SPDI':6,'SPFI':7}
 fuel_system_encode=fuel_dict[fuel_system]
 
-compression_ratio=st.number_input(label='Enter Compression Ratio',min_value=7.00,max_value=23.00,value=7.00,step=1.,format="%.2f")
-horse_power=st.number_input(label='Enter Horse Power',min_value=48,max_value=288,value=48)
-peak_rpm=st.number_input(label='Enter Peak rpm',min_value=4150,max_value=6600,value=4150)
-city_mpg=st.number_input(label='Enter City Mileage',min_value=13,max_value=49,value=13)
-highway_mpg=st.number_input(label='Enter Highway Mileage',min_value=16,max_value=54,value=16)
 
 
+compression_ratio=st.number_input(label='Enter Compression Ratio',value=7.00,step=1.,format="%.2f")
+if(compression_ratio>23 or compression_ratio<7):
+    st.warning("Please enter the value of Compression Ratio between 7 and 23")
+    flagcr=1
+else:
+    flagcr=0
 
+horse_power=st.number_input(label='Enter Horse Power',value=48)
+if(horse_power>288 or horse_power<48):
+    st.warning("Please enter the value of Horse Power between 48 and 288")
+    flaghp=1
+else:
+    flaghp=0
+
+peak_rpm=st.number_input(label='Enter Peak rpm',value=4150)
+if(peak_rpm>6600 or peak_rpm<4150):
+    st.warning("Please enter the value of Peak rpm between 4150 and 6600")
+    flagpr=1
+else:
+    flagpr=0
+
+city_mpg=st.number_input(label='Enter City Mileage',value=13)
+if(city_mpg>49 or city_mpg<13):
+    st.warning("Please enter the value of City Mileage between 13 and 49")
+    flagcm=1
+else:
+    flagcm=0
+
+highway_mpg=st.number_input(label='Enter Highway Mileage',value=16)
+if(highway_mpg>54 or highway_mpg<16):
+    st.warning("Please enter the value of Higheay Mileage between 16 and 54")
+    flaghm=1
+else:
+    flaghm=0
 
 #gender=st.selectbox('Select Gender',['Male','Female'])
 submit_btn =st.button("Show Expected Price of vehicle")
 
 if submit_btn==True:
-    intake=[[fuel_type_encode,aspiration_encode,door_number_encode,car_body_encode,drive_wheel_encode,engine_location_encode,engine_type_encode,cylinder_number_encode,fuel_system_encode,compression_ratio,horse_power,peak_rpm,city_mpg,highway_mpg]]
-    result=model.predict(intake)[0]
-    result=result*80
-    st.write("The predicted price is ",result)
+    if(flagcr!=0 or flagcm!=0 or flaghm!=0 or flaghp!=0 or flagpr!=0):
+        st.write("Kindly enter appropriate input values input values")
+    else:
+        intake=[[fuel_type_encode,aspiration_encode,door_number_encode,car_body_encode,drive_wheel_encode,engine_location_encode,engine_type_encode,cylinder_number_encode,fuel_system_encode,compression_ratio,horse_power,peak_rpm,city_mpg,highway_mpg]]
+        result=model.predict(intake)[0]
+        result=result*80
+        st.write("The predicted price is ",result)
